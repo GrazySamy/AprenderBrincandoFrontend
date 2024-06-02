96b8790ca423ba8490e31ac418e92232aefb02bf
analivalmeida <analivialpalmeida@gmail.com>
2024-06-01 22:36:16 -0300

    Adição de vídeos nas páginas Alfabetização e Matemática

--- basic/basictools/src/pages/EnsinoFundamental/Matematica/Matematica.jsx
+++ basic/basictools/src/pages/EnsinoFundamental/Matematica/Matematica.jsx
@@ -4,92 +4,92 @@ import FaixaTitulo from '../../../components/FaixaTitulo/FaixaTitulo'
 const Matematica = () => {
   return (
     <>
-    <FaixaTitulo colorClass="faixa-matematica" titulo="ENSINO FUNDAMENTAL" subtitulo="Matemática" />
+      <FaixaTitulo colorClass="faixa-matematica" titulo="ENSINO FUNDAMENTAL" subtitulo="Matemática" />
 
-    <div class="row py-5">
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/QaeeOFT31eU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
+      <div class="row py-5">
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/a-z9hFrI5jI?si=cyRfqj5aCoMZBdhI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Ditado de números com caixa de ovos
+              </p>
             </div>
           </div>
         </div>
-        <div class="row pb-5">
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/QaeeOFT31eU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6 col-lg-3">
-            <div class="card h-100, w-100 ">
-              <iframe width="100%" height="185" src="https://www.youtube.com/embed/0PZ8wgnMGCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <div class="card-body">
-                <p class="card-text">
-                  texto
-                </p>
-              </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/B7xwO6bzYWk?si=25bBHdFbdOND42UR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade de Números e Quantidade com copos
+              </p>
             </div>
           </div>
         </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/Lu_HUZBsxJ0?si=8NmpOOl30r1ny5HT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade lúdica - tabuada
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/R2AyMOVfSCk?si=xTi-5yulYyfscbLC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Aprendendo números com dominó
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row pb-5">
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/qvX38V6N8lI?si=7eACYJoLnhTRvdhy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade - Contando laranjas
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/H1EhiGwE1FU?si=uHebW5VU3uyfrV4W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade - Ajudar o aprendizado com discalculia
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+            <iframe width="457" height="315" src="https://www.youtube.com/embed/eQ097mBwxVs?si=l9Dlsxo6Cuy1vDwY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade - Números e Quantidades
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <div class="card h-100, w-100 ">
+          <iframe width="457" height="315" src="https://www.youtube.com/embed/HNvbs0qC6bE?si=NshEKoKzxP2U4ob0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+              <p class="card-text">
+                Atividade - Auxiliar o ensino da tabuada
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
