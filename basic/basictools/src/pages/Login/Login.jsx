dcc6e254939064583292de21bee0be27d54674ce
GuRodri <gustavo_rodrigues_silva@hotmail.com>
2024-06-16 10:31:43 -0300

    Uso de Firebase-Tools para Deploy

--- basic/basictools/src/pages/Login/Login.jsx
+++ basic/basictools/src/pages/Login/Login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
           {!loading && <button className="btn">Entrar com</button>}
           {loading && <button className="btn" disabled>Aguarde...</button>}
           {error && <p className='error'>{error}</p>}
-          <button type="button" className={styles.button} onClick={handleGoogleLogin} disabled={loading}>
+          <button type="button" class="btn btn-outline-primary" onClick={handleGoogleLogin} disabled={loading}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 58 40" width="20px" height="20px">
             <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
               <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
